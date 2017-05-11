f6151692c0378c18eade76cd53d6f43cbfa4a04f
lrh <lrh@13021131633.com>
2017-05-11 03:11:14 -0400

    5.11

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import numpy as np
+import csv
+
+
+def getTrainData():
+    fp = open("trainMF","r") 
+    n_user = 6041
+    n_item = 3953
+
+    R_matrix = np.zeros([n_user,n_item],dtype=np.int32)
+
+    lines = list(csv.reader(fp))
+
+    for line in lines:
+        user = int(line[0])
+        n = len(line)
+        for i in range(0,n):
+            item = int(line[2*i+1])
+            rating = int(line[2*i+2])
+            R_matrix[user][item] = rating
+
+    fp.close()
+    return R_matrix
+
+def getUserDistance():
+    pass
+
+def getItemDistance():
+    pass
+
+
+
+    
